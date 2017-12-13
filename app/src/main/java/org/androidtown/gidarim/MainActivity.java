93611fea5af65d0d8c2be0e95d553d3ab93c59b4
devilzCough <devilzCough@gmail.com>
2017-12-13 21:12:40 +0900

    add Edit Activity make Card

--- app/src/main/java/org/androidtown/gidarim/MainActivity.java
+++ app/src/main/java/org/androidtown/gidarim/MainActivity.java
@@ -1,7 +1,11 @@
 package org.androidtown.gidarim;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.settings);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int curId = item.getItemId();
+        switch(curId) {
+            case R.id.menu_add:
+                Toast.makeText(this, "                .", Toast.LENGTH_SHORT).show(); break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
